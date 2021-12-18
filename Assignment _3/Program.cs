d3e71bb4bc64852e9c468bf84dbea3de55396d1c
moyeras <95027542+moyeras@users.noreply.github.com>
2021-12-18 14:37:44 +0100

    Add files via upload

---
+++ Assignment _3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        
+        List<int> NoPrime = new List<int>();
+        List<int> Prime = new List<int>();
+
+        Console.WriteLine("write a number");
+        int number = int.Parse(Console.ReadLine());
+
+        for (int x = 2; x <= number; x++)
+        {
+            for (int y = x * 2; y <= number; y = y + x)
+            {
+
+                if (!NoPrime.Contains(y))
+                {
+                    NoPrime.Add(y);
+                }
+            }      
+        }
+        for (int z = 2; z <= number; z++)
+        {
+            if (!NoPrime.Contains(z))
+            {
+                Prime.Add(z);
+            }
+        }
+
+        List<int> uniqueListInt = Prime.Distinct().ToList();
+
+        foreach (int primes in uniqueListInt)
+        {
+            System.Console.Write("{0} ", primes);
+        }
+        Console.ReadLine();
+    }
+}
